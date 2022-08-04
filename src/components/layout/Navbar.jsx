16fd7f3aab073e75606b228edfcd0a705204b58a
TRAN BAO ANH THU <tranbaoanhthu@Audrey-Trump.local>
2022-08-04 21:35:37 +0700

    update code

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Logo from'../../assets/imgs/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
-        Navbar
-    </nav>
+   
+    
+       
+  
   )
 }
 
